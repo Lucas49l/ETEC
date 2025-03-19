8e95164aac89b07fb78c19f13619be20cecc6aec
Lucas Guimarães <lucasguimaraes1649@gmail.com>
2025-03-19 11:15:17 -0300

    Add files via upload

---
+++ 1º Modulo/Programação e Algoritmos/Aulas/02 - 25/js/1° Nome e idade.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite o Nome");
+var idade = prompt("Digite a idade");
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
---
+++ 1º Modulo/Programação e Algoritmos/Aulas/02 - 25/js/3° Média.js
@@ -0,0 +1,6 @@
+var primeiraNota = parseFloat(prompt("1° Nota: "));
+var segundaNota = parseFloat(prompt("2° Nota: "));
+var terceiraNota = parseFloat(prompt("3° Nota: "));
+
+var media = (primeiraNota+segundaNota+terceiraNota)/3;
+console.log("Média: ",media);
