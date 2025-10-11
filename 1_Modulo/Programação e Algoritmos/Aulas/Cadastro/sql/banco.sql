47bb9f9b666a77addb77da04b8e022c2125aa637
Lucas49l <lucasguimaraes1649@gmail.com>
2025-10-11 14:49:23 -0300

    Organizando Pastas

--- 1_Modulo/Programação e Algoritmos/Aulas/Cadastro/sql/banco.sql
+++
@@ -1,6 +0,0 @@
-Create table usuarios (
-ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
-nome Varchar(100)
-login Varchar(30),
-senha Varchar(40),
-Primary Key (ID)) ENGINE = MyISAM;
--- 1_Modulo/Programação e Algoritmos/Outros/DOM/Lista_com_id/class.js
+++
@@ -1,3 +0,0 @@
-const lista = document.querySelectorAll(".valores") /*pegar todas as classes*/
-
-console.log(lista)
